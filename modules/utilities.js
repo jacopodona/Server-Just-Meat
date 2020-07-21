791fd47a328f4b535a9b1f6132fcc665cc5a1194
davide <davide.farina.1998@gmail.com>
2020-07-21 11:53:00 +0200

    bugfix

--- modules/utilities.js
+++ modules/utilities.js
@@ -5,14 +5,11 @@ exports.verifyEmail = function verifyEmail(email) {
 };
 
 exports.verifyToken = function verifyToken(req, res, next) {
-  next();
-  // Commented function for debug only.
-  // Uncomment once client-side login has been implemented and delete previous line.
-  /*const bearerHeader = req.headers['authorization'];
+  const bearerHeader = req.headers['authorization'];
   if(bearerHeader) {
     req.token = bearerHeader.split(' ')[1];
     next();
   } else {
     res.status(403).send('Forbidden');
-  }*/
+  }
 }
