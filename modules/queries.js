c3b8c06483c9510e520dcebef3afa5c52cd23c0d
davide <davide.farina.1998@gmail.com>
2020-08-27 09:56:26 +0200

    bugfix

--- modules/queries.js
+++ modules/queries.js
@@ -57,7 +57,7 @@ const get_supermarkets = (offset, limit) => {
 
 const get_products = (id_supermercato, limit, offset) => {
   return {
-    text: 'SELECT P.id AS id, P.name AS name, price, barcode, discount, image, description, D.id AS department, M.name AS manufacturer, W.um AS um, W.value AS weight, W.id AS fk_weight, LP.fk_user AS favourite FROM products P JOIN has_product H ON P.id = H.fk_product JOIN supermarkets S ON H.fk_supermarket = S.id JOIN departments D ON D.id = H.fk_department JOIN manufacturers M ON M.id = P.fk_manufacturer JOIN has_weight HW ON HW.fk_product=P.id JOIN weights W ON HW.fk_weight=W.id LEFT JOIN love_products LP ON LP.fk_product = P.id WHERE S.id = 4 AND W.value = (SELECT MIN(W1.value) FROM weights W1 JOIN has_weight HW1 ON W1.id=HW1.fk_weight JOIN products P1 ON P1.id=HW1.fk_product WHERE P1.id=P.id) ORDER BY id LIMIT $2 OFFSET $3',
+    text: 'SELECT P.id AS id, P.name AS name, price, barcode, discount, image, description, D.id AS department, M.name AS manufacturer, W.um AS um, W.value AS weight, W.id AS fk_weight, LP.fk_user AS favourite FROM products P JOIN has_product H ON P.id = H.fk_product JOIN supermarkets S ON H.fk_supermarket = S.id JOIN departments D ON D.id = H.fk_department JOIN manufacturers M ON M.id = P.fk_manufacturer JOIN has_weight HW ON HW.fk_product=P.id JOIN weights W ON HW.fk_weight=W.id LEFT JOIN love_products LP ON LP.fk_product = P.id WHERE S.id = $1 AND W.value = (SELECT MIN(W1.value) FROM weights W1 JOIN has_weight HW1 ON W1.id=HW1.fk_weight JOIN products P1 ON P1.id=HW1.fk_product WHERE P1.id=P.id) ORDER BY id LIMIT $2 OFFSET $3',
     values: [id_supermercato, limit, offset]
   }
 }
